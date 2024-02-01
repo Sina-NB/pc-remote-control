b0d1a2a051fd176929b5139b4537a40e0d945dbe
Sina Nabigol <sina.nabigol77@gmail.com>
2024-02-02 02:49:08 +0330

    Add Shutdown command

--- core/bot.py
+++ core/bot.py
@@ -5,23 +5,28 @@
 
 import telebot
 import yaml
+import os
 
 with open('settings.yml', 'r') as f:
     data = yaml.full_load(f)
 API_TOKEN = data['token']
-print(API_TOKEN)
+print(f'Starting on token: {API_TOKEN}')
 
-bot = telebot.TeleBot('6929643510:AAF7sILSALJWnSSYj8ymEJqNhHIz1MKRYqQ')
+bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    print('recieved')
-#     bot.reply_to(message, """\
-# Hi there, I am EchoBot.
-# I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-# """)
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['shutdown'])
+def send_welcome(message):
+    bot.reply_to(message, 'Your PC Shutdowns')
+    os.system("shutdown now -h")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
